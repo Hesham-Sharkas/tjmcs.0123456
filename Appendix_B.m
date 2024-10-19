958279ace6baf8dba7e1b0236bd1619f5b847013
Hesham Sharkas <sharkas147@yahoo.com>
2024-10-19 08:56:29 +0300

    Matlab files

---
+++ Appendix_B.m
@@ -0,0 +1,18 @@
+clc;
+clear;
+
+N = 256;
+u = 2*rand(1,N,'double')-1 + 2i*rand(1,N,'double')-1i;
+v = 2*rand(1,N,'double')-1 + 2i*rand(1,N,'double')-1i;
+
+u_hat = DFT(u)./sqrt(N);
+v_hat = DFT(v)./sqrt(N);
+
+LHS = CyclicConvolution(u_hat,v_hat);
+RHS = sqrt(N) .* DFT(u.*v)./sqrt(N);
+
+display_output_nicely = zeros(N,2);
+display_output_nicely(:,1) = transpose(LHS);
+display_output_nicely(:,2) = transpose(RHS);
+
+disp(display_output_nicely);  %The output is shown in two parralel coloumns
