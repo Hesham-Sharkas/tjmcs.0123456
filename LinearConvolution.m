5c76ec7bd10c5bdf73327d6cd79aa36412bf55e2
Hesham Sharkas <sharkas147@yahoo.com>
2024-12-06 09:43:46 +0300

    v01.01

--- LinearConvolution.m
+++ LinearConvolution.m
@@ -1,3 +1,7 @@
+% Written by Hesham Sharkas, on 22 October, 2024
+% This code implements Linear Convolution, used in theorems and corollaries
+% in the open access article DOI: http://dx.doi.org/10.47000/tjmcs.1424850
+
 function out = LinearConvolution(u,v)
 ul = length(u);
 vl = length(v);
