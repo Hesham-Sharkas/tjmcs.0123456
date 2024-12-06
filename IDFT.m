b33d014cff86fa31afe43628b54a2a15f1c5d06c
Hesham Sharkas <sharkas147@yahoo.com>
2024-12-06 09:39:56 +0300

    v01.01

--- IDFT.m
+++ IDFT.m
@@ -1,3 +1,7 @@
+% Written by Hesham Sharkas, on 22 October, 2024
+% This code implements IDFT -NOT Weighted- used in theorems and corollaries
+% in the open access article DOI: http://dx.doi.org/10.47000/tjmcs.1424850
+
 function x = IDFT(x_hat)
 N = length(x_hat);
 k = 0 : N-1;
